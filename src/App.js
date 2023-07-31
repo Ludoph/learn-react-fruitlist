f1d821015b3515b2e788ef70de5b37295050be87
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-31 16:11:03 +0200

    feat: create dynamic list only delete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import { useState } from "react";
 
 function App() {
   //State (état, données)
-  const [compteur, setCompteur] = useState(1);
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" },
+  ]);
 
   //Comportements
-  const handleClick = () => {
-    //alert("handleClick");
-    setCompteur(compteur + 1);
+  const handleDelette = (id) => {
+    console.log(id);
+    //1. copie mon state
+    const fruitsCopy = fruits.slice();
+
+    //2. manipuler la copie de mon state
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+
+    //3. modifier mon state avec le setter
+    setFruits(fruitsCopyUpdated);
   };
 
   //Affichage (render)
-  return <div></div>;
+  return (
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit.id}>
+              {fruit.nom}
+              <button onClick={() => handleDelette(fruit.id)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
